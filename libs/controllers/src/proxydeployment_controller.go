343b4119c2437bff981a4d03dc556655563539ee
Jérémy Levilain <jeremy@jeremylvln.fr>
2022-10-21 11:55:53 +0200

    refactor: split packages folder

---
+++ libs/controllers/src/proxydeployment_controller.go
@@ -0,0 +1,222 @@
+/*
+Copyright (c) Jérémy Levilain
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"hash/fnv"
+
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/rand"
+	hashutil "k8s.io/kubernetes/pkg/util/hash"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	shulkermciov1alpha1 "github.com/iamblueslime/shulker/libs/crds/v1alpha1"
+	common "github.com/iamblueslime/shulker/libs/resources/src"
+	resources "github.com/iamblueslime/shulker/libs/resources/src/proxydeployment"
+)
+
+const templateHashLabel = "proxydeployment.shulkermc.io/template-hash"
+
+// ProxyDeploymentReconciler reconciles a ProxyDeployment object
+type ProxyDeploymentReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=shulkermc.io,resources=proxies,verbs=get;list;watch;create;update
+//+kubebuilder:rbac:groups=shulkermc.io,resources=proxydeployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=shulkermc.io,resources=proxydeployments/status,verbs=get;update;patch
+
+func (r *ProxyDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	logger.Info("Reconciling ProxyDeployment")
+	proxyDeployment, err := r.getProxyDeployment(ctx, req.NamespacedName)
+
+	if client.IgnoreNotFound(err) != nil {
+		return ctrl.Result{}, err
+	} else if k8serrors.IsNotFound(err) {
+		// No need to requeue if the resource no longer exists
+		return ctrl.Result{}, nil
+	}
+
+	cluster := &shulkermciov1alpha1.MinecraftCluster{}
+	err = r.Get(ctx, types.NamespacedName{
+		Namespace: proxyDeployment.Namespace,
+		Name:      proxyDeployment.Spec.ClusterRef.Name,
+	}, cluster)
+	if err != nil {
+		logger.Error(err, "Referenced MinecraftCluster does not exists")
+		return ctrl.Result{}, err
+	}
+
+	resourceBuilder := resources.ProxyDeploymentResourceBuilder{
+		Instance: proxyDeployment,
+		Scheme:   r.Scheme,
+	}
+	builders, dirtyBuilders := resourceBuilder.ResourceBuilders()
+
+	err = ReconcileWithResourceBuilders(r.Client, ctx, builders, dirtyBuilders)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	allProxies, err := r.getAllProxies(ctx, proxyDeployment)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if len(allProxies.Items) > int(proxyDeployment.Spec.Replicas) {
+		nbToRemove := len(allProxies.Items) - int(proxyDeployment.Spec.Replicas)
+
+		for i := 0; i < nbToRemove; i += 1 {
+			err = r.Delete(ctx, &allProxies.Items[i])
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	} else if len(allProxies.Items) < int(proxyDeployment.Spec.Replicas) {
+		templateHash := getProxyTemplateHash(&proxyDeployment.Spec.Template)
+		matchingProxies, err := r.getMatchingProxies(ctx, proxyDeployment, templateHash)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		proxiesToCreate := int(proxyDeployment.Spec.Replicas) - len(matchingProxies.Items)
+		for i := 0; i < proxiesToCreate; i += 1 {
+			proxyId := common.RandomResourceId(6)
+			proxy := shulkermciov1alpha1.Proxy{}
+
+			labels := r.getProxyLabelsWithTemplateHash(proxyDeployment, templateHash)
+			for k, v := range proxyDeployment.Spec.Template.Labels {
+				labels[k] = v
+			}
+
+			proxy.Namespace = proxyDeployment.Namespace
+			proxy.Name = fmt.Sprintf("%s-%s-%s", proxyDeployment.Name, templateHash, proxyId)
+			proxy.Labels = labels
+			proxy.Spec = proxyDeployment.Spec.Template.Spec
+			proxy.Spec.ClusterRef = proxyDeployment.Spec.ClusterRef
+			proxy.Spec.Configuration = shulkermciov1alpha1.ProxyConfigurationSpec{
+				ExistingConfigMapName: resourceBuilder.GetConfigMapName(),
+			}
+
+			if err := controllerutil.SetControllerReference(proxyDeployment, &proxy, r.Scheme); err != nil {
+				err = fmt.Errorf("failed setting controller reference for Proxy: %v", err)
+				return ctrl.Result{}, err
+			}
+
+			err = r.Create(ctx, &proxy)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	allProxies, err = r.getAllProxies(ctx, proxyDeployment)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	availableReplicas, unavailableReplicas := 0, 0
+	for _, proxy := range allProxies.Items {
+		for _, condition := range proxy.Status.Conditions {
+			if shulkermciov1alpha1.ProxyStatusCondition(condition.Type) == shulkermciov1alpha1.ProxyReadyCondition {
+				if condition.Status == metav1.ConditionTrue {
+					availableReplicas += 1
+				} else {
+					unavailableReplicas += 1
+				}
+			}
+		}
+	}
+
+	selector, err := metav1.LabelSelectorAsSelector(resourceBuilder.GetPodSelector())
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	proxyDeployment.Status.Replicas = proxyDeployment.Spec.Replicas
+	proxyDeployment.Status.AvailableReplicas = int32(availableReplicas)
+	proxyDeployment.Status.UnavailableReplicas = int32(unavailableReplicas)
+	proxyDeployment.Status.Selector = selector.String()
+
+	if availableReplicas > 0 {
+		proxyDeployment.Status.SetCondition(shulkermciov1alpha1.ProxyDeploymentAvailableCondition, metav1.ConditionTrue, "AtLeastOneReady", "One or more proxies are ready")
+	} else {
+		proxyDeployment.Status.SetCondition(shulkermciov1alpha1.ProxyDeploymentAvailableCondition, metav1.ConditionFalse, "NotReady", "No proxy is ready")
+	}
+
+	return ctrl.Result{}, r.Status().Update(ctx, proxyDeployment)
+}
+
+func (r *ProxyDeploymentReconciler) getProxyDeployment(ctx context.Context, namespacedName types.NamespacedName) (*shulkermciov1alpha1.ProxyDeployment, error) {
+	proxyDeployment := &shulkermciov1alpha1.ProxyDeployment{}
+	err := r.Get(ctx, namespacedName, proxyDeployment)
+	return proxyDeployment, err
+}
+
+func (r *ProxyDeploymentReconciler) getProxyLabels(deployment *shulkermciov1alpha1.ProxyDeployment) map[string]string {
+	labels := map[string]string{
+		"minecraftcluster.shulkermc.io/name": deployment.Spec.ClusterRef.Name,
+		"proxydeployment.shulkermc.io/name":  deployment.Name,
+	}
+	return labels
+}
+
+func (r *ProxyDeploymentReconciler) getProxyLabelsWithTemplateHash(deployment *shulkermciov1alpha1.ProxyDeployment, templateHash string) map[string]string {
+	labels := r.getProxyLabels(deployment)
+	labels[templateHashLabel] = templateHash
+	return labels
+}
+
+func (r *ProxyDeploymentReconciler) getAllProxies(ctx context.Context, deployment *shulkermciov1alpha1.ProxyDeployment) (*shulkermciov1alpha1.ProxyList, error) {
+	list := shulkermciov1alpha1.ProxyList{}
+	err := r.List(ctx, &list, client.InNamespace(deployment.Namespace), client.MatchingLabels(r.getProxyLabels(deployment)))
+
+	return &list, err
+}
+
+func (r *ProxyDeploymentReconciler) getMatchingProxies(ctx context.Context, deployment *shulkermciov1alpha1.ProxyDeployment, templateHash string) (*shulkermciov1alpha1.ProxyList, error) {
+	list := shulkermciov1alpha1.ProxyList{}
+	err := r.List(ctx, &list, client.InNamespace(deployment.Namespace), client.MatchingLabels(r.getProxyLabelsWithTemplateHash(deployment, templateHash)))
+
+	return &list, err
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ProxyDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	err := mgr.GetFieldIndexer().IndexField(context.Background(), &shulkermciov1alpha1.ProxyDeployment{}, ".spec.clusterRef.name", func(object client.Object) []string {
+		proxyDeployment := object.(*shulkermciov1alpha1.ProxyDeployment)
+		return []string{proxyDeployment.Spec.ClusterRef.Name}
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&shulkermciov1alpha1.ProxyDeployment{}).
+		Owns(&corev1.Service{}).
+		Owns(&shulkermciov1alpha1.Proxy{}).
+		Complete(r)
+}
+
+func getProxyTemplateHash(template *shulkermciov1alpha1.ProxyTemplate) string {
+	hasher := fnv.New32a()
+	hashutil.DeepHashObject(hasher, *template)
+
+	return rand.SafeEncodeString(fmt.Sprint(hasher.Sum32()))
+}
---
+++ libs/resources/src/proxy/configmap.go
@@ -0,0 +1,168 @@
+/*
+Copyright (c) Jérémy Levilain
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+package resources
+
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	shulkermciov1alpha1 "github.com/iamblueslime/shulker/libs/crds/v1alpha1"
+)
+
+const defaultServerIcon = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAG1BMVEUAAABSNGCrhKqXaZdsSGtDJlCEUomPY4/////HT7OpAAAACXRSTlMA//////////83ApvUAAAAh0lEQVRYhe3Qyw6AIAxEURTQ//9jmYQmE1IeWwbvytD2LAzhb9JFnQTw0U0tIxsD3lGkUmmIbA7wYRwEJJdUgac0A7qIEDBCEqUKYMlDkpMqgENG8MZHb00ZQBgYYoAdYmZvqoA94tsOsNzOVAHUA3hZHfCW2uVc6x4fACwlABiy9MOEgaP6APk1HDGFXeaaAAAAAElFTkSuQmCC"
+
+type ProxyResourceConfigMapBuilder struct {
+	*ProxyResourceBuilder
+}
+
+func (b *ProxyResourceBuilder) ProxyConfigMap() *ProxyResourceConfigMapBuilder {
+	return &ProxyResourceConfigMapBuilder{b}
+}
+
+func (b *ProxyResourceConfigMapBuilder) Build() (client.Object, error) {
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      b.GetConfigMapName(),
+			Namespace: b.Instance.Namespace,
+			Labels:    b.getLabels(),
+		},
+	}, nil
+}
+
+func (b *ProxyResourceConfigMapBuilder) Update(object client.Object) error {
+	configMap := object.(*corev1.ConfigMap)
+
+	configMapData, err := GetConfigMapDataFromConfigSpec(&b.Instance.Spec.Configuration)
+	if err != nil {
+		return err
+	}
+	configMap.Data = configMapData
+
+	if err := controllerutil.SetControllerReference(b.Instance, configMap, b.Scheme); err != nil {
+		return fmt.Errorf("failed setting controller reference for ConfigMap: %v", err)
+	}
+
+	return nil
+}
+
+func (b *ProxyResourceConfigMapBuilder) CanBeUpdated() bool {
+	return true
+}
+
+type configServerYml struct {
+	Motd       string `yaml:"motd"`
+	Address    string `yaml:"address"`
+	Restricted bool   `yaml:"restricted"`
+}
+
+type configListenerYml struct {
+	Host               string   `yaml:"host"`
+	QueryPort          int16    `yaml:"query_port"`
+	Motd               string   `yaml:"motd"`
+	MaxPlayers         int32    `yaml:"max_players"`
+	Priorities         []string `yaml:"priorities"`
+	PingPassthrough    bool     `yaml:"ping_passthrough"`
+	ForceDefaultServer bool     `yaml:"force_default_server"`
+	ProxyProtocol      bool     `yaml:"proxy_protocol"`
+}
+
+type configYml struct {
+	Servers                 map[string]configServerYml `yaml:"servers"`
+	Listeners               []configListenerYml        `yaml:"listeners"`
+	Groups                  map[string]interface{}     `yaml:"groups"`
+	OnlineMode              bool                       `yaml:"online_mode"`
+	IpForward               bool                       `yaml:"ip_forward"`
+	PreventProxyConnections bool                       `yaml:"prevent_proxy_connections"`
+	EnforceSecureProfile    bool                       `yaml:"enforce_secure_profile"`
+}
+
+func getConfigYmlFile(spec *shulkermciov1alpha1.ProxyConfigurationSpec) (string, error) {
+	configYml := configYml{
+		Servers: map[string]configServerYml{
+			"limbo": {
+				Motd:       spec.Motd,
+				Address:    "localhost:25565",
+				Restricted: false,
+			},
+		},
+		Listeners: []configListenerYml{{
+			Host:               "0.0.0.0:25577",
+			QueryPort:          int16(25577),
+			Motd:               spec.Motd,
+			MaxPlayers:         spec.MaxPlayers,
+			Priorities:         []string{"limbo"},
+			PingPassthrough:    false,
+			ForceDefaultServer: true,
+			ProxyProtocol:      spec.ProxyProtocol,
+		}},
+		Groups:                  map[string]interface{}{},
+		OnlineMode:              true,
+		IpForward:               true,
+		PreventProxyConnections: true,
+		EnforceSecureProfile:    true,
+	}
+
+	out, err := yaml.Marshal(&configYml)
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
+
+func GetConfigMapDataFromConfigSpec(spec *shulkermciov1alpha1.ProxyConfigurationSpec) (map[string]string, error) {
+	configMapData := make(map[string]string)
+
+	configMapData["init-fs.sh"] = trimScript(`
+		#!/bin/bash
+		set -euo pipefail
+		cp "$SHULKER_CONFIG_DIR/probe-liveness.sh" "$SHULKER_DATA_DIR/probe-liveness.sh"; chmod +x "$SHULKER_DATA_DIR/probe-liveness.sh"
+		cp "$SHULKER_CONFIG_DIR/probe-readiness.sh" "$SHULKER_DATA_DIR/probe-readiness.sh"; chmod +x "$SHULKER_DATA_DIR/probe-readiness.sh"
+		if [ -e "$SHULKER_CONFIG_DIR/server-icon.png" ]; then cat $SHULKER_CONFIG_DIR/server-icon.png | base64 -d > $SHULKER_DATA_DIR/server-icon.png; fi
+		if [ -e "$SHULKER_CONFIG_DIR/config.yml" ]; then cp "$SHULKER_CONFIG_DIR/config.yml" "$SHULKER_DATA_DIR/config.yml"; fi
+	`)
+
+	configMapData["probe-liveness.sh"] = trimScript(`
+		#!/bin/bash
+		set -euo pipefail
+		bash /health.sh
+	`)
+
+	configMapData["probe-readiness.sh"] = trimScript(`
+		#!/bin/bash
+		set -euo pipefail
+		if ! test -f "${SHULKER_DRAIN_LOCK_DIR}/no-drain"; then exit 1; fi
+		bash /health.sh
+	`)
+
+	if spec.ServerIcon != "" {
+		configMapData["server-icon.png"] = spec.ServerIcon
+	} else {
+		configMapData["server-icon.png"] = defaultServerIcon
+	}
+
+	configYml, err := getConfigYmlFile(spec)
+	if err != nil {
+		return configMapData, err
+	}
+	configMapData["config.yml"] = configYml
+
+	return configMapData, nil
+}
+
+func trimScript(script string) string {
+	lines := strings.Split(strings.TrimSpace(script), "\n")
+	for i := range lines {
+		lines[i] = strings.TrimSpace(lines[i])
+	}
+	return strings.Join(lines, "\n")
+}
