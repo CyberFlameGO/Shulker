a4dfc35cc45a573a389b1c63f49adb5f7bf80e8d
Jérémy Levilain <jeremy@jeremylvln.fr>
2022-10-21 14:25:55 +0200

    ci: update Nx action

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -6,11 +6,11 @@ labels="$3"
 
 tags="$(echo "${tags}" | sed "s/##SHULKER_APP##/${app}/g")"
 if [ "${tags}" != "" ]; then
-  tags_params="--tag $(echo "${tags}" | sed "s/,/ --tag /g")"
+  tags_params="--tag $(echo "${tags}" | sed "s/;;;/ --tag /g")"
 fi
 
 if [ "${labels}" != "" ]; then
-  labels_params="--label $(echo "${labels}" | sed "s/,/ --label /g")"
+  labels_params="--label $(echo "${labels}" | sed "s/;;;/ --label /g")"
 fi
 
 docker build \
